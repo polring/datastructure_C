8a60a29b81e7c0416599f472fee01a9c6e4cbefa
polring <katack8313@gmail.com>
2025-04-20 21:37:59 +0900

    polynomial multply

--- 4.14/test.c
+++ 4.14/test.c
@@ -1,104 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct Node{
     int coef;
     int expon;
-}polynomial;
-
-typedef struct Node{
-    polynomial data;
     struct Node* next;
 }node;
 
-
-node* createNode(polynomial data){
+node* createNode(int coef,int expon){
     node* newnode = malloc(sizeof(node));
-    newnode->data = data;
     newnode->next = NULL;
+    newnode->coef = coef;
+    newnode->expon = expon;
     return newnode;
 }
+void append(node **head,int coef,int expon){
+    node* newnode = createNode(coef,expon);
+    node *temp = *head;
+    if(*head == NULL){
+        *head = newnode;
+        return;
+    }
+    while(temp->next){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+}
 
-void insert(polynomial data,node **head,node *ptr){
-    node *newnode = createNode(data);
-    if(*head == NULL || ptr == NULL){
+void sortedinsert(node **head,int coef,int expon){
+    node *newnode= createNode(coef,expon);
+    if(*head == NULL){
+        *head = newnode;
+        return;
+    }
+    if((*head)->expon < expon){
         newnode->next = *head;
         *head = newnode;
         return;
     }
-    newnode->next = ptr->next;
-    ptr->next = newnode;
-}
-node *last(node *head){
-    if(head == NULL){
-        return NULL;
+    node *temp = *head;
+    while(temp->next && temp->next->expon > expon){
+        temp = temp->next;
     }
-    while(head->next){
-        head = head->next;
+    if(temp->next == NULL){
+        temp->next = newnode;
+        return;
     }
-    return head;
-}
-void scan(const char* name,node **head){
-    FILE *fp = fopen(name,"r");
-    polynomial data;
-    int n;
-    fscanf(fp,"%d",&n);
-    for(int i=0;i<n;i++){
-        fscanf(fp,"%d %d",&data.coef,&data.expon);
-        insert(data,head,last(*head));
+    node *temp3 = temp->next;
+    if(temp->next->expon == expon){
+        temp->next->coef += coef;
+        if(temp->next->coef == 0){
+            temp->next = temp->next->next;
+            free(temp3);
+        }
+        free(newnode);
+        return;
     }
-    
+    newnode->next = temp->next;
+    temp->next = newnode;
 }
-void print(node *head){
+
+void multiply(node** head){
+    node* trail1 = head[0];
+    node* trail2 = head[1];
+    int coef,expon;
+    while(trail1){
+        trail2 = head[1];
+        while(trail2){
+            coef = trail2->coef * trail1->coef;
+            expon = trail2->expon + trail1->expon;
+            sortedinsert(head + 2,coef,expon);
+            trail2 = trail2->next;
+        }
+        trail1 = trail1 ->next;
+    }
+}
+void printlist(node *head){
     while(head){
-        printf("[%d, %d] ",head->data.coef,head->data.expon);
+        printf("[%d %d] ",head->coef,head->expon);
         head = head->next;
     }
     printf("\n");
 }
-node *compare(node *head,int expon){
-    if(head == NULL){
-        return NULL;
+void freeList(node **head){
+    node *temp = *head;
+    while(*head){
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
     }
-    while(head->next && head->next->data.expon >= expon ){
-        if(head->next->data.expon == expon ){
-            return head->next;
-        }
-        head = head->next;
-        
-    }
-    
-    return head;
+    *head = NULL;
 }
-
-void multiply(node **head){
-    polynomial data;
-    node *ptr;
-    node *trail = head[0];
-    node *trail2 = head[1];
-    while(trail){
-        trail2 = head[1];
-        while(trail2){
-            data.coef = trail->data.coef * trail2->data.coef;
-            data.expon = trail->data.expon + trail2->data.expon;
-            ptr = compare(head[2],data.expon);
-            if(ptr->data.expon == data.expon){
-                ptr->data.coef +=data.coef;
-            }
-            insert(data,head+2,ptr);
-            trail2 = trail2->next;
-        }
-        trail = trail->next;
+void execute(const char* name1,const char* name2){
+    FILE *fp = fopen(name1,"r");
+    FILE *fp2 = fopen(name2,"r");
+    node *head[3] = {NULL,NULL,NULL};
+    int n;
+    int coef,expon;
+    fscanf(fp,"%d",&n);
+    for(int i=0;i<n;i++){
+        fscanf(fp,"%d %d",&coef,&expon);
+        append(head,coef,expon);
     }
+    fscanf(fp2,"%d",&n);
+    for(int i=0;i<n;i++){
+        fscanf(fp2,"%d %d",&coef,&expon);
+        append(head+1,coef,expon);
+    }
+    multiply(head);
+    printf("poly:\n");
+    printlist(head[0]);
+    printf("poly2:\n");
+    printlist(head[1]);
+    printf("poly1 * poly2:\n");
+    printlist(head[2]);
+    fclose(fp);
+    fclose(fp2);
+    freeList(head);
+    freeList(head + 1);
+    freeList(head + 2);
+    
 }
 
 int main(){
-    node *head[3] = {NULL,NULL,NULL};
-    scan("in1.txt",head);
-    scan("in2.txt",head+1);
-    multiply(head);
-    print(head[2]);
-
-
+    execute("in1.txt","in2.txt");
+    execute("in3.txt","in4.txt");
 }
-
